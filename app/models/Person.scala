6eca39bf4c7253fcbb8882dfdfd8cd61e9c46e88
abteilung6 <moeller.tilman@gmail.com>
2021-01-06 18:06:23 +0100

    define Person Table object

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -1,3 +1,13 @@
 package models
 
+import slick.jdbc.MySQLProfile.api._
+
 case class Person (id: Long = 0L, username: String)
+
+class PersonTable(tag: Tag) extends Table[Person](tag, "person") {
+
+  def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def username = column[String]("username")
+
+  def * = (id, username).mapTo[Person]
+}
