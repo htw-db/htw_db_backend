dc03c0297327a5c5a8c828dd1264dcbcb65548b1
abteilung6 <moeller.tilman@gmail.com>
2021-01-09 17:40:06 +0100

    PersonController dont need Await anymore

--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -5,8 +5,6 @@ import models.Person
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc._
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 import services.PersonService
 
 class PersonController @Inject()(cc: ControllerComponents, personService: PersonService) extends AbstractController(cc) {
@@ -14,7 +12,7 @@ class PersonController @Inject()(cc: ControllerComponents, personService: Person
   implicit val personFormat: OFormat[Person] = Json.format[Person]
 
   def getAll: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    val persons = Await.result(personService.listPersons(), Duration.Inf)
+    val persons = personService.listPersons()
     Ok(Json.toJson(persons))
   }
 }
