de759ed0ca351e1abb9a15e5b04958d768388735
abteilung6 <moeller.tilman@gmail.com>
2021-01-09 17:43:20 +0100

    sync person after ldap success

--- app/controllers/AccountController.scala
+++ app/controllers/AccountController.scala
@@ -1,18 +1,32 @@
 package controllers
 
 import javax.inject._
-import models.Person
+import models.{CredentialsForm, CredentialsFormData, Person}
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc._
+import services.{LdapService, PersonService}
 
-import services.LdapService
-
-class AccountController @Inject()(cc: ControllerComponents, ldapService: LdapService) extends AbstractController(cc) {
+class AccountController @Inject()(cc: ControllerComponents, ldapService: LdapService, personService: PersonService) extends AbstractController(cc) {
 
   implicit val personFormat: OFormat[Person] = Json.format[Person]
 
   def authenticate: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    val result = ldapService.authenticate("", "")
-    Ok("ok")
+    CredentialsForm.form.bindFromRequest.fold(
+      errorForm => {
+        errorForm.errors.foreach(println)
+        BadRequest(errorForm.errors.head.message)
+      },
+      credentials => {
+        val isLdapAuthenticated = ldapService.authenticate(credentials.username, credentials.password)
+        if (isLdapAuthenticated) {
+          val person = personService.findPersonByUsername(credentials.username)
+          if (person.isEmpty) {
+            val addedPerson = personService.addPerson(credentials.username)
+          }
+          Ok("ok")
+        } else {
+          Unauthorized("")
+        }
+      })
   }
 }
