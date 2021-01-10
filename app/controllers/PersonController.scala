284ff485581b326cf7f80e71b96bd7437c765531
abteilung6 <moeller.tilman@gmail.com>
2021-01-10 20:10:28 +0100

    use UserAction in person controller

--- app/controllers/PersonController.scala
+++ app/controllers/PersonController.scala
@@ -1,17 +1,17 @@
 package controllers
 
+import actions.{AuthAction, UserRequest}
 import javax.inject._
 import models.Person
 import play.api.libs.json.{Json, OFormat}
 import play.api.mvc._
-
 import services.PersonService
 
-class PersonController @Inject()(cc: ControllerComponents, personService: PersonService) extends AbstractController(cc) {
+class PersonController @Inject()(cc: ControllerComponents, personService: PersonService, authAction: AuthAction) extends AbstractController(cc) {
 
   implicit val personFormat: OFormat[Person] = Json.format[Person]
 
-  def getAll: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  def getAll: Action[AnyContent] = authAction { implicit request: UserRequest[AnyContent] =>
     val persons = personService.listPersons()
     Ok(Json.toJson(persons))
   }
