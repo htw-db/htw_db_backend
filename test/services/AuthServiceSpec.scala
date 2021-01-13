c08a0dcf67f2df754cbf3bf8597d6f8b9914e269
abteilung6 <moeller.tilman@gmail.com>
2021-01-13 11:53:40 +0100

    fix AuthServiceSpec with mocking Configuraiton

--- test/services/AuthServiceSpec.scala
+++ test/services/AuthServiceSpec.scala
@@ -1,23 +1,25 @@
 package services
 
 import org.scalatestplus.play.PlaySpec
+import play.api.Configuration
 import play.api.libs.json.Json
 
 class AuthServiceSpec extends PlaySpec {
 
   "AuthServiceSpec#createToken" should {
 
+    val mockConfig: Configuration = Configuration(("jwt.secretKey", "mockSecretKey"))
     val jwtRegex = """(.+?)\.(.+?)\.(.+?)""".r
 
     "create a JSON Web Token" in {
-      val authService = new AuthService()
+      val authService = new AuthService(mockConfig)
       val claim = Json.obj(("username", "alice"))
       val actual = authService.createToken(claim)
       actual must fullyMatch regex jwtRegex
     }
 
     "validate a valid JSON Web Token" in {
-      val authService = new AuthService()
+      val authService = new AuthService(mockConfig)
       val claim = Json.obj(("username", "alice"))
       val jsonWebToken = authService.createToken(claim)
       val actual = authService.isValidToken(jsonWebToken)
@@ -25,17 +27,17 @@ class AuthServiceSpec extends PlaySpec {
     }
 
     "validate a invalid JSON Web Token" in {
-      val authService = new AuthService()
+      val authService = new AuthService(mockConfig)
       val actual = authService.isValidToken("")
       actual mustBe false
     }
 
     "get claim from valid JSON Web Token" in {
-      val authService = new AuthService()
+      val authService = new AuthService(mockConfig)
       val claim = Json.obj(("username", "alice"))
       val jsonWebToken = authService.createToken(claim)
       val actual = authService.decodeClaim(jsonWebToken).getOrElse(Json.obj())
-      actual must equal (claim)
+      actual must equal(claim)
     }
   }
 }
